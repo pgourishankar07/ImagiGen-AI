abaf6cb012a5c10bc450dc638611f7539b14a7e2
Gouri Shankar <gourishankar@Gouris-MacBook-Air.local>
2024-08-09 06:22:41 +0530

    Changed the api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  // baseURL: "http://localhost:8080/api/",
+  baseURL: "https://imagigen-ai-backend-gouri-shankar.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
